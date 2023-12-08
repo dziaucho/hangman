3445650afb745f92e3570b404ae419f0e51f2caf
Maryja Harshunova <spiakota@outlook.com>
2023-12-08 23:19:20 +0500

    feat: kinda paused animation

--- coffee-house/scripts/script.js
+++ coffee-house/scripts/script.js
@@ -36,7 +36,15 @@ let nextButton = document.querySelector('.next');
 let controlItmes = document.querySelectorAll('.active');
 
 let counter = 0;
+
 let widthCarousel;
+let intervalTimeAnimation;
+let ongoingControl;
+
+let checkTimeout = 0;
+let checkInterval;
+let counterTime = 0;
+let paused = 0;
 
 function resizeCarousel() {
   widthCarousel = carousel.offsetWidth;
@@ -48,13 +56,23 @@ function resizeCarousel() {
   moveRight();
 }
 
+function Test() {
+  checkTimeout++;
+  if (checkTimeout > 5) {
+    checkTimeout = 1;
+  }
+  console.log('regular', checkTimeout)
+}
+
 function moveRightWithCounter() {
   counter++;
   if (counter >= coffeeCards.length) {
     counter = 0
   };
+  clearInterval(intervalTimeAnimation);
   removeOngoingControl();
   controlItmes[counter].classList.add('ongoing');
+  intervalTimeAnimation = setInterval(moveRightWithCounter, 5000);
   carouselLine.style.transform = 'translate(-' + counter * widthCarousel + 'px)';
 }
 
@@ -67,6 +85,10 @@ function moveLeft() {
   if (counter < 0) {
     counter = coffeeCards.length - 1;
   };
+  clearInterval(intervalTimeAnimation);
+  removeOngoingControl();
+  controlItmes[counter].classList.add('ongoing');
+  intervalTimeAnimation = setInterval(moveRightWithCounter, 5000);
   carouselLine.style.transform = 'translate(-' + counter * widthCarousel + 'px)';
 }
 
@@ -78,10 +100,39 @@ function removeOngoingControl() {
   }
 }
 
+function touchOnCarousel() {
+  ongoingControl = document.querySelector('.ongoing');
+  ongoingControl.style.animationPlayState = 'paused';
+  checkInterval = setInterval(checkTouchInterval, 1000);
+}
+
+function touchOnCarouselEnd() {
+  ongoingControl = document.querySelector('.ongoing');
+  ongoingControl.style.animationPlayState = 'running';
+  paused = (5 - Math.abs(counterTime - checkTimeout));
+  console.log('result', paused)
+  setTimeout(moveRightWithCounter, paused)
+}
+
+function checkTouchInterval() {
+  counterTime += 1;
+  if (counterTime > 5) {
+    counterTime = 1;
+  }
+  console.log(counterTime)
+}
+
 window.addEventListener('resize', resizeCarousel);
-resizeCarousel();
 
 nextButton.addEventListener('click', moveRightWithCounter);
 preveousButton.addEventListener('click', moveLeft);
 
-setInterval(moveRightWithCounter, 5000);
+for (let i = 0; i < coffeeCards.length; i++) {
+  coffeeCards[i].addEventListener('touchstart', touchOnCarousel);
+  coffeeCards[i].addEventListener('touchend', touchOnCarouselEnd);
+}
+
+resizeCarousel();
+intervalTimeAnimation = setInterval(moveRightWithCounter, 5000);
+
+setInterval(Test, 1000)
