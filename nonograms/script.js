0cc0584a6ac7bf9eb92373806d03a135f77769be
maryja-harshunova <spiakota@outlook.com>
2024-02-06 00:50:27 +0500

    feat: choose game

--- nonograms/script.js
+++ nonograms/script.js
@@ -36,8 +36,14 @@ let tasks = [
     [0, 0, 1, 0, 0],
     [0, 1, 0, 1, 0],
     [1, 1, 0, 1, 0]
+  ],
+  [
+    [0, 0, 0, 1, 1],
+    [0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 1],
+    [1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1]
   ]
-
 ]
 
 let emptyNonogram = [
@@ -75,6 +81,13 @@ let buttonRestart = makeElement("button", "button-restart");
 let buttonRandom = makeElement("button", "button-random");
 let buttonSwitchTheme = makeElement("button", "button-switch-theme");
 
+let chooseGameWrapper = makeElement("div", "choose-game-wrapper");
+let game1 = makeElement("p", "game-1 game-paragraph");
+let game2 = makeElement("p", "game-2 game-paragraph");
+let game3 = makeElement("p", "game-3 game-paragraph");
+let game4 = makeElement("p", "game-4 game-paragraph");
+let game5 = makeElement("p", "game-5 game-paragraph");
+
 let tipsCols = getTipsCols(currentGame);
 let tipsRows = getTipsRows(currentGame);
 let lengthCols = getLongestColTip(tipsCols);
@@ -107,6 +120,19 @@ gameSection.appendChild(buttonsWrapper);
 buttonsWrapper.appendChild(buttonRestart);
 buttonsWrapper.appendChild(buttonRandom);
 buttonsWrapper.appendChild(buttonSwitchTheme);
+
+gameSection.appendChild(chooseGameWrapper);
+chooseGameWrapper.appendChild(game1);
+chooseGameWrapper.appendChild(game2);
+chooseGameWrapper.appendChild(game3);
+chooseGameWrapper.appendChild(game4);
+chooseGameWrapper.appendChild(game5);
+
+game1.innerHTML = "1 game";
+game2.innerHTML = "2 game";
+game3.innerHTML = "3 game";
+game4.innerHTML = "4 game";
+game5.innerHTML = "5 game";
 
 buttonRestart.innerHTML = "restart";
 buttonRandom.innerHTML = "random";
@@ -204,7 +230,7 @@ function restartGame() {
     ceilsClick[i].classList.remove("chosen");
     ceilsClick[i].classList.remove("offcast");
     timeMinutes = 0;
-    timerCounter = 0;
+    timerCounter = -1;
     checkCorrect();
   }
 }
@@ -214,6 +240,10 @@ function restartGame() {
 buttonRandom.addEventListener("click", randomGame);
 
 function randomGame() {
+  timeMinutes = 0;
+  timerCounter = -1;
+  checkCorrect();
+
   ceilsMatrix = [
     [],
     [],
@@ -267,6 +297,74 @@ function randomGame() {
     ceilsClick[i].addEventListener("contextmenu", checkOffcast(i));
     ceilsClick[i].addEventListener("click", checkCorrect);
   }
-  console.log(ceilsMatrix)
+}
 
+// choose game
+
+let gameOptions = document.querySelectorAll("p");
+
+for (let i = 0; i < gameOptions.length; i += 1) {
+  gameOptions[i].addEventListener("click", chooseGame(i));
+}
+
+function chooseGame(item) {
+  return function () {
+    timeMinutes = 0;
+    timerCounter = -1;
+    checkCorrect();
+
+    ceilsMatrix = [
+      [],
+      [],
+      [],
+      [],
+      []
+    ];
+    gameZoneCol1.removeChild(tableTipsRows);
+    gameZoneCol2.removeChild(tableTipsCols);
+    gameZoneCol2.removeChild(table);
+
+    currentGame = tasks[item];
+
+    table = makeTable(currentGame.length);
+
+    tipsCols = getTipsCols(currentGame);
+    tipsRows = getTipsRows(currentGame);
+
+    tableTipsCols = makeTipsTable(lengthCols, currentGame.length);
+    tableTipsRows = makeTipsTable(currentGame.length, lengthRows);
+
+    gameZoneCol1.appendChild(tableTipsRows);
+    gameZoneCol2.appendChild(tableTipsCols);
+    gameZoneCol2.appendChild(table);
+
+    rows = tableTipsCols.querySelectorAll("tr");
+    start = rows.length - 1;
+
+    pushColsTips(rows, tipsCols, start)
+
+    columns = tableTipsRows.querySelectorAll("tr");
+    for (let i = 0; i < columns.length; i += 1) {
+      let ceils = Array.from(columns[i].querySelectorAll("td"));
+      ceils.reverse();
+      pushRowsTips(ceils, tipsRows[i]);
+    }
+
+    ceilsClick = table.querySelectorAll("td");
+
+    counter = 0;
+
+    for (let i = 0; i < ceilsClick.length; i += 5) {
+      for (let j = i; j < i + 5; j += 1) {
+        ceilsMatrix[counter].push(ceilsClick[j]);
+      }
+      counter += 1;
+    }
+
+    for (let i = 0; i < ceilsClick.length; i += 1) {
+      ceilsClick[i].addEventListener("click", checkChosen(i));
+      ceilsClick[i].addEventListener("contextmenu", checkOffcast(i));
+      ceilsClick[i].addEventListener("click", checkCorrect);
+    }
+  }
 }
